5d0240021ccfd8fa71e1c2ebaffa362021795ff4
Max Droog Hayes <maxdrooghayes@hotmail.com>
2016-06-26 10:01:20 +0100

    sleeeeep

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from naoqi import ALProxy
 import urllib
 from clarifai.client import ClarifaiApi
+import time
 
 tts = ALProxy("ALTextToSpeech", "172.20.10.4", 9559)
 mem = ALProxy("ALMemory", "172.20.10.4", 9559)
 
-# get the image
-tts.say("Getting image")
-myURL = mem.getData("LanguageLearner/ImageURL")
-urllib.urlretrieve(myURL, "C:/Users/Max/Documents/UKNAO/PepperPic.jpg")
+int currVal = 0
 
-# image processing
-tts.say("Processing image")
-clarifai_api = ClarifaiApi() # assumes environment variables are set.
-result = clarifai_api.tag_images(open('/path/to/local/image.jpeg', 'rb'))
-resultList = result['results'][0]['result']['tag']['classes']
+while(true):
 
-# Return the result to Pepper
-tts.say("I think this is an apple")
-mem.insertData("LanguageLearner/Object",resultList[0])
+	 newVal = changemem.getData("LanguageLearner/Index")
+	
+	if  newVal > currVal :
+		currVal = newVal
+		doStuff()
+
+	time.sleep(10)
+		
+		
+def  doStuff() :
+	# get the image
+	tts.say("Getting image")
+	myURL = mem.getData("LanguageLearner/ImageURL")
+	urllib.urlretrieve(myURL, "C:/Users/Max/Documents/UKNAO/PepperPic.jpg")
+
+	# image processing
+	tts.say("Processing image")
+	clarifai_api = ClarifaiApi() # assumes environment variables are set.
+	result = clarifai_api.tag_images(open('/path/to/local/image.jpeg', 'rb'))
+	resultList = result['results'][0]['result']['tag']['classes']
+
+	# Return the result to Pepper
+	tts.say("I think this is a " + str(resultsList[0]))
+	mem.insertData("LanguageLearner/Object",resultList[0])
