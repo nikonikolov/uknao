854286ae5407390551d2a45ead5edd888dcc139c
Max Droog Hayes <maxdrooghayes@hotmail.com>
2016-06-26 13:30:16 +0100

    update

--- main.py
+++ main.py
@@ -1,28 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from naoqi import ALProxy
 import urllib
 from clarifai.client import ClarifaiApi
 import time
 
 def  doStuff() :
+
 	# get the image
-	tts.say("Getting image")
+	tts.say("Downloading image")
 	myURL = mem.getData("LanguageLearner/ImageURL")
 	urllib.urlretrieve(myURL, "C:/Users/Max/Documents/My Stuff/UK-NAO-hackathon/PepperPic.jpg")
 
 	# image processing
-	tts.say("Processing image")
-	clarifai_api = ClarifaiApi() # assumes environment variables are set.
+	tts.say("Hmmmm let me see")
+	clarifai_api = ClarifaiApi(language="fr") # assumes environment variables are set.
 	result = clarifai_api.tag_images(open( "C:/Users/Max/Documents/My Stuff/UK-NAO-hackathon/PepperPic.jpg", 'rb'))
 	resultList = result['results'][0]['result']['tag']['classes']
+	print resultList
 
 	# Return the result to Pepper
-	print str(resultList[0])
-	tts.say("I think this is a " + str(resultList[0]))
-	mem.insertData("LanguageLearner/Object",resultList[0])
+	#print str(resultList[0])
+	#tts.say("I think this is a " + str(resultList[0]))
+	print "sending word to Pepper:"
+	
+	try:
+		mem.insertData("LanguageLearner/Object",str(resultList[0]))
+		print resultList[0]
+	except:
+		mem.insertData("LanguageLearner/Object",str(resultList[1]))
+		print resultList[1]
+		
 
 
 tts = ALProxy("ALTextToSpeech", "172.20.10.4", 9559)
 mem = ALProxy("ALMemory", "172.20.10.4", 9559)
+
+mem.insertData("LanguageLearner/Index",0)
 
 currVal = 0
 
@@ -32,7 +47,7 @@ while(True):
 	
 	if  newVal > currVal :
 		currVal = newVal
-		print currVal
+		print  "currVal is "  + str(currVal )+ '\n'
 		doStuff()
 
-	time.sleep(10)
+	time.sleep(5)
